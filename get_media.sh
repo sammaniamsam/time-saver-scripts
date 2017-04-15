8d17f238f4f9929c3ba885701123ec79f11ac6c1
Samuel Sikes <sammaniamsam@github.com>
2017-04-15 14:52:16 -0500

    initial commit of get_media

---
+++ get_media.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+
+#--------------------------------------
+#Print specific error message and
+#display propper command usage
+#
+# Arguments:
+#   message
+# Returns:
+#   None
+#--------------------------------------
+
+print_error_message() {
+	printf "\n****************************************\n"
+	printf "$1\n"
+	print_command_arg_options
+	printf "\n****************************************\n\n"
+}
+
+#--------------------------------------
+#Print correct command usage
+#
+# Arguments:
+#   None
+# Returns:
+#   None
+#--------------------------------------
+
+print_command_arg_options() {
+	printf "pics only <command> -p\n"
+	printf "footage only <command> -f\n"
+	printf "pics and footage <command> -pf OR -fp"
+}
+
+#--------------------------------------
+#Get volume name to extract footage from
+#
+# Arguments:
+#   volume_name
+# Returns:
+#   None
+#--------------------------------------
+
+get_user_selected_volume() {
+	printf "\n****************************************"
+	printf "\nSELECT WHICH VOLUME TO GET MEDIA FROM: \n"
+	printf "Volumes:  "; ls /Volumes/
+	printf "SELECTED VOLUME: " ; read $1
+	printf "****************************************\n"
+}
+
+#--------------------------------------
+#Validate that the user selected volume exists
+#
+# Arguments:
+#   volume_name
+# Returns:
+#   None
+#--------------------------------------
+
+check_if_valid_volume() {
+	while [ $(find "/Volumes" -maxdepth 1 -name "$1" | wc -l) -eq 0 ] ; do
+		printf "RE-ENTER SELECTED VOLUME: " ; read volume_name
+		set -- "$volume_name"
+	done
+}
+
+#--------------------------------------
+#Executes appropriate command option
+#
+# Arguments:
+#   command_options -[pf]
+# Returns:
+#   None
+#--------------------------------------
+
+execute_command_option() {
+	case "$1" in
+		-f) extract_footage $2 ;;
+		-p) extract_pictures $2 ;;
+		-*) extract_pictures $2
+			extract_footage $2
+	esac
+}
+
+#--------------------------------------
+#Executes appropriate footage methods
+#
+# Arguments:
+#   volume_name
+# Returns:
+#   None
+#--------------------------------------
+
+extract_footage() {
+	local footage_file_formats="mov|MOV|mp4|MP4"
+	create_media_directory "Footage"
+	create_directory_for_media $1 "Footage" $footage_file_formats
+	prompt_user_about_deleting_media $1 "Footage" $footage_file_formats
+}
+
+#--------------------------------------
+#Executes appropriate picture methods
+#
+# Arguments:
+#   volume_name
+# Returns:
+#   None
+#--------------------------------------
+
+extract_pictures() {
+	local pic_file_formats="jpg|JPG|jpeg|JPEG|png|PNG"
+	create_media_directory "Pictures"
+	create_directory_for_media $1 "Pictures" $pic_file_formats
+	prompt_user_about_deleting_media $1 "Pictures" $pic_file_formats
+}
+
+#--------------------------------------
+#Create a media directory in home directory
+#
+# Arguments:
+#   media_directory
+# Returns:
+#   None
+#--------------------------------------
+
+create_media_directory() {
+	if [ $(find $HOME -maxdepth 1 -name "$1" | wc -l) -eq 0 ]
+	then
+		mkdir $HOME/"$1"
+	fi
+}
+
+#--------------------------------------
+#Get user to create a directory in
+#which to store their media
+#
+# Arguments:
+#   volume_name, media_directory, file_extension_filters
+# Returns:
+#   None
+#--------------------------------------
+
+create_directory_for_media() {
+	printf "\n****************************************"
+	printf "\nSELECT NAME FOR NEW $2 DIRECTORY\n"
+	local dir_nm; printf "DIRECTORY NAME: "; read dir_nm
+	while [ $(find "$HOME/$2/" -maxdepth 1 -name "$dir_nm" | wc -l) -gt 0 ] ; do
+		printf "DIRECTORY EXISTS! RE-ENTER DIRECTORY NAME: " ; read dir_nm
+	done
+	mkdir "$HOME/$2/$dir_nm"
+	copy_media_to_user_created_directory $1 $2 $3
+	printf "****************************************\n"
+}
+
+#--------------------------------------
+#Copy all media to new user created directory
+#
+# Arguments:
+#   volume_name, media_directory, file_extension_filters
+# Returns:
+#   None
+#--------------------------------------
+
+copy_media_to_user_created_directory() {
+	printf "COPYING $2...\n"
+	find -E "/Volumes/$1" -iregex ".*\.($3)" -exec cp "{}" "$HOME/$2/$dir_nm" \;
+}
+
+#--------------------------------------
+#Ask user if they want to delete
+#the copied media from the volume
+#they selected
+#
+# Arguments:
+#   volume_name, media_directory, file_extension_filters
+# Returns:
+#   None
+#--------------------------------------
+
+prompt_user_about_deleting_media() {
+	printf "\n****************************************"
+	printf "\nREMOVE ALL $2 FROM VOLUME {Y or N} ?: "; read answer
+	while [[ $answer != [YyNn] ]] ; do
+		printf "REMOVE ALL $2 FROM VOLUME?: "; read answer
+	done
+	if [[ $answer == [Yy] ]]
+	then
+		delete_media $1 $2 $3
+	fi
+	printf "****************************************\n"
+}
+
+#--------------------------------------
+#Delete media from volume
+#
+# Arguments:
+#   volume_name, media_directory, file_extension_filters
+# Returns:
+#   None
+#--------------------------------------
+
+delete_media() {
+	printf "REMOVING ALL $2...\n"
+	find -E "/Volumes/$1" -iregex ".*\.($3)" -exec rm "{}" 2> /dev/null \;
+}
+
+#--------------------------------------
+#Validate get_media command options and
+#invoke appropriate methods
+#
+# Arguments:
+#   command_options -[pf]
+# Returns:
+#   None
+#--------------------------------------
+
+main() {
+	if [ $# -eq 1 ];
+	then isValidArg="^-(p|f|fp|pf){1}$"
+		if [[ $1 =~ $isValidArg ]];
+		then local volume_name
+			get_user_selected_volume $volume_name
+			check_if_valid_volume $volume_name
+			execute_command_option $1 $volume_name
+		else print_error_message "INVALID ARGUMENTS"
+		fi
+	else print_error_message "INCORRECT NUMBER OF ARGUMENTS"
+	fi
+}
+
+main "$@"
